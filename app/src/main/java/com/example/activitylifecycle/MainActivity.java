4d9980b5203357f0a4db2ce1837518af8041d9f6
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2020-11-30 10:37:53 +0545

    lifecycle lab completed

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.activitylifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -9,18 +10,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String TAG= "MainActivity";
 
     private Button DialogButton;
-
     private Button SecondButton;
+    int sum=0;
+    private TextView  textView;
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart()");
+        Log.d(TAG, "onStart");
     }
 
     @Override
@@ -30,24 +33,36 @@ public class MainActivity extends AppCompatActivity {
 
         DialogButton=findViewById(R.id.button);
         SecondButton=findViewById(R.id.button2);
+        textView=findViewById(R.id.textView);
 
-        Log.d(TAG, "onCreate()");
+        Log.d(TAG, "onCreate");
+        Log.d(TAG,"sum: "+sum);
+
+        if(savedInstanceState!=null)
+        {
+            sum=savedInstanceState.getInt("SUMKEY");
+            Log.d(TAG,"sum after rotation:"+sum);
+            textView.setText(""+sum);
+        }
 
         DialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AlertDialog.Builder(  MainActivity.this)
+               /* new AlertDialog.Builder(  MainActivity.this)
                         .setTitle("Delete Entry")
                         .setMessage("Are you sure you want to delete this entry?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
 
                             }
                         })
-                        .setNegativeButton(android.R.string.no, null)
+                        .setNegativeButton(android.R.string.cancel, null)
                         .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                        .show();*/
+                sum = sum+1;
+                Log.d(TAG,"sum: "+sum);
+                textView.setText(""+sum);
             }
         });
 
@@ -62,26 +77,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt("SUMKEY", sum);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume()");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause()");
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop()");
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy()");
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "onBackPressed");
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/activitylifecycle/SecondActivity.java
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.java
@@ -1,49 +1,48 @@
 package com.example.activitylifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
-    public final static String TAG= "SecondActivity ";
+    public final static String TAG= "SecondActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Log.d(TAG, "onCreate()");
+        Log.d(TAG, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart()");
+        Log.d(TAG, "onStart");
     }
-
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume()");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause()");
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop()");
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy()");
+        Log.d(TAG, "onDestroy");
     }
-
 }
